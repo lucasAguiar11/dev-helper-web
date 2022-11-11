b65f91ab25398d170ab473c6f259ebcb8243c684
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-11-11 17:28:12 -0300

    projeto base

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import Header from '../components/Header'
+import BackgroundSVG from '../components/svgs/BackgroundSVG'
+
+export default function Example() {
+  return (
+    <>
+      <div className="isolate bg-white">
+        <Header />
+        {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+          <BackgroundSVG className="w-screen h-screen" />
+        </div> */}
+        <main>
+          <div className="relative px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+              <div>
+                <div>
+                  <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                    Data to enrich your online business
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+                    commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                  </p>
+                  <div className="mt-8 flex gap-x-4 sm:justify-center">
+                    <a
+                      href="#"
+                      className="inline-block rounded-lg bg-primary px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-primary hover:bg-secondary hover:ring-indigo-700"
+                    >
+                      Get started
+                      <span className="text-indigo-200" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </a>
+                    <a
+                      href="#"
+                      className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                    >
+                      Live demo
+                      <span className="text-gray-400" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+    },
+    colors: ({ colors }) => ({
+      ...colors,
+      primary: '#5c65c0',
+      secondary: '#6f95ff',
+    }),
+
+    extend: {},
+  },
+  plugins: [],
+}
