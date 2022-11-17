c533eb8278e225bc30c18f9f815d9d70ffae9e78
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-11-17 09:09:48 -0300

    nova maneira de importar gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,16 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+            }}
+          />
         </body>
       </Html>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -11,59 +10,41 @@ export default function Home() {
         </div> */}
         <main>
           <div className="px-6 lg:px-8">
-            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-32 sm:pb-40">
-              <div>
-                <div>
-                  <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                    O melhor amigo de um Dev aqui :)
-                  </h1>
-                  <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                    Confira aqui os melhores{' '}
-                    <span className="text-primary font-bold">&quot;helpers&quot;</span> para o seu
-                    dia a dia. 😁
-                  </p>
-                  <div className="mt-8 flex gap-x-4 sm:justify-center">
-                    <Link
-                      href="/helpers?section=validator"
-                      className="inline-block rounded-lg bg-primary px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-primary hover:bg-primary/80 hover:ring-primary"
-                    >
-                      Preciso Validar
-                      <span className="text-gray-200" aria-hidden="true">
-                        &rarr;
-                      </span>
-                    </Link>
+            <div className="mx-auto flex flex-col max-w-3xl pt-20 pb-32 sm:pt-32 sm:pb-40">
+              <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                O melhor amigo de um Dev aqui :)
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                Confira aqui os melhores{' '}
+                <span className="text-primary font-bold">&quot;helpers&quot;</span> para o seu dia a
+                dia. 😁
+              </p>
+              <div className="mt-8 flex gap-x-4 sm:justify-center">
+                <Link
+                  href="/helpers?section=validator"
+                  className="inline-block rounded-lg bg-primary px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-primary hover:bg-primary/80 hover:ring-primary"
+                >
+                  Preciso Validar
+                  <span className="text-gray-200" aria-hidden="true">
+                    &rarr;
+                  </span>
+                </Link>
 
-                    <Link
-                      href="/helpers?section=generator"
-                      className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                    >
-                      Quero Gerar
-                      <span className="text-gray-200" aria-hidden="true">
-                        &rarr;
-                      </span>
-                    </Link>
-                  </div>
-                </div>
+                <Link
+                  href="/helpers?section=generator"
+                  className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                >
+                  Quero Gerar
+                  <span className="text-gray-200" aria-hidden="true">
+                    &rarr;
+                  </span>
+                </Link>
               </div>
+              <div className="h-full"></div>
             </div>
           </div>
         </main>
       </div>
-      <Script
-        async
-        id="ads-script-1"
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADSENSE_CLIENT}`}
-      />
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-7889378758959768"
-        data-ad-slot="8186002461"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-
-      <Script id="ads-script-2">(adsbygoogle = window.adsbygoogle || []).push({});</Script>
     </>
   )
 }
