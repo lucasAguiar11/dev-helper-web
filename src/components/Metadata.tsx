07cafb9e119222ad4820d901dda68b6d4ef86e69
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-11-18 09:48:03 -0300

    melhoria de SEO

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -9,8 +9,8 @@ type MetadataProps = {
 }
 
 export const Metadata = ({
-  title = 'Dev Helper | O melhor amigo de um Dev aqui :)',
-  description = 'Encontre aqui as melhores ferramentas para desenvolvedores',
+  title = 'O melhor amigo de um Dev aqui :)',
+  description = 'Encontre aqui as melhores ferramentas online para desenvolvedores, com nosso geradores e validadores de dados.',
   canonical,
 }: MetadataProps) => {
   const router = useRouter()
@@ -20,6 +20,21 @@ export const Metadata = ({
         <meta charSet="UTF-8" key="charset" />
         <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
         <link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@" />
+        <meta name="twitter:title" content="O melhor amigo de um Dev aqui :) | Dev Helper " />
+        <meta name="twitter:creator" content="@" />
+        <meta name="author" content="Lucas Aguiar" />
+        <meta
+          name="keywords"
+          content="geradores, gerar, validar, validadores, helpes, gerar cpf, validar cpf, gerar cnpj, validar cnpj, helper"
+        />
+
+        <meta
+          name="twitter:description"
+          content="Encontre aqui as melhores ferramentas online para desenvolvedores, com nosso geradores e validadores de dados."
+        />
         <link
           rel="icon"
           type="image/png"
@@ -38,15 +53,41 @@ export const Metadata = ({
       </Head>
 
       <NextSeo
+        titleTemplate="%s | Dev Helper"
+        defaultTitle="O melhor amigo de um Dev aqui :)"
         title={title}
         description={description}
         canonical={canonical}
+        robotsProps={{
+          noarchive: false,
+          nosnippet: false,
+          notranslate: false,
+          noimageindex: false,
+          maxImagePreview: 'large',
+          maxSnippet: -1,
+          maxVideoPreview: -1,
+        }}
         openGraph={{
           title: title,
           description: description,
           url: canonical,
           locale: 'pt-BR',
           site_name: 'Dev Helper',
+          article: {
+            authors: ['Lucas Aguiar'],
+            tags: [
+              'geradores',
+              'gerar',
+              'validar',
+              'validadores',
+              'helpes',
+              'gerar cpf',
+              'validar cpf',
+              'gerar cnpj',
+              'validar cnpj',
+              'helper',
+            ],
+          },
         }}
       />
     </>
